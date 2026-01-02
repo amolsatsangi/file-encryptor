a18d38cdc1f50d3dc72b87a7c88a70c8af211b56
amolsatsangi <15amolsat@gmail.com>
2026-01-02 04:45:18 +0000

    Remove task serialization and log real task metadata

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -15,7 +15,7 @@ void ProcessManagement::executeTask(){
     while(!taskQueue.empty()){
         std::unique_ptr<Task> task_to_execute = std::move(taskQueue.front());
         taskQueue.pop();
-        std::cout<<"Executing task: "<<task_to_execute->toString()<<std::endl;
-        executeCryption(task_to_execute->toString());
+        std::cout<<"Executing task: "<<task_to_execute->filePath<<std::endl;
+        executeCryption(*task_to_execute);
     }
 }
--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -5,47 +5,20 @@
 #include<string>
 #include<fstream>
 #include "../fileHandling/IO.hpp"
-#include<sstream>
 
 enum class Action{
     ENCRYPT,
     DECRYPT
 };
 
-struct Task{
-    std::string filePath;
-    std::fstream f_stream;
-    Action action;
-
-    Task(std::fstream && f_str,std::string fp,  Action act):
-        f_stream{std::move(f_str)},filePath{fp},action{act}{}
-    
-    std::string toString(){
-        std::ostringstream oss;
-        oss<<filePath<<","<<(action==Action::ENCRYPT?"ENCRYPT":"DECRYPT");
-        //"test.txt","ENCRYPT"
-        return oss.str();
-    }
-    static Task fromString(const std::string &taskData){
-        std::istringstream iss(taskData);
+class Task{
+    public:
         std::string filePath;
-        std::string actionStr;
+        std::fstream f_stream;
+        Action action;
 
-        if(std::getline(iss,filePath,',') && std::getline(iss,actionStr)){
-            Action action = (actionStr == "ENCRYPT"?Action::ENCRYPT: Action::DECRYPT);
-            IO io(filePath);
-            std::fstream f_stream = std::move(io.getFileStream());
-            if(f_stream.is_open()){
-                return Task(std::move(f_stream),filePath,action);
-            }
-            else{
-                throw std::runtime_error("Failed to open file: "+ filePath);
-            }
-        }
-        else{
-            throw std::runtime_error("Invalid Task format");
-        }
-    }
+        Task(std::fstream && f_str,std::string fp,  Action act):
+            f_stream{std::move(f_str)},filePath{fp},action{act}{}
 }; 
 
 
